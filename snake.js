2e7527a5ac954178ef1366bf955616bf0494bf22
MichaelLeonffu <Leonffu.Michael@gmail.com>
2017-07-25 22:29:47 -0700

    Added cool Printing feature

--- snake.js
+++ snake.js
@@ -11,24 +11,39 @@ stdin.addListener("data", function(d) {
 
 var symbols = {
 	borders:{
-		leftRight: '|',
-		upDown: '='
+		leftRight: '\033[0m|',
+		upDown: '\033[0m='
 	},
-	blankSpace: '+',
-	apple: 'a',
-	snakeHead: '@',
-	snakeTail: 'O'
+	blankSpace: '\033[0;31m+',
+	apple: '\033[0ma',
+	snakeHead: '\033[0m@',
+	snakeTail: '\033[0mO'
+}
+var directions = {
+	up: 1,
+	down: 2,
+	left: 3,
+	right: 4
 }
 
-function generateEmptyField(hieght,width){
+function wait(timeInMilli){
+	var duration = Math.abs(timeInMilli)
+	var startTime = new Date()
+	var currentTime = 0
+	while(currentTime - startTime <= duration){currentTime = new Date()}
+}
+
+function generateEmptyField(height,width){
 	var table = []
-	for (var row = 0; row <= hieght; row++) {
+	for (var row = 0; row <= height; row++) {
 		table[row] = []
 		for (var column = 0; column <= width; column++) {
-			if(row == 0 || row == hieght){
+			if(row === 0 || row === height){
 				table[row][column] = symbols.borders.upDown
-			}else if(column == 0 || column == width){
+			}else if(column === 0 || column === width){
 				table[row][column] = symbols.borders.leftRight
+			}else if(column === Math.round(width/2) && row === Math.round(height/2)){
+				table[row][column] = symbols.snakeHead
 			}else{
 				table[row][column] = symbols.blankSpace
 			}
@@ -37,6 +52,28 @@ function generateEmptyField(hieght,width){
 	return table
 }
 
+function snakeController(field,snake,direction){
+	const DEFAULTCORDS = {
+		row: 0,
+		column: 0
+	}
+	var snake = {
+		headPOS: DEFAULTCORDS,
+		tailPOS: DEFAULTCORDS
+	}
+	findingSnakeHead:
+	for (var row = 0; row <= field.length; row++) {
+		for (var column = 0; column <= field[row].length; column++) {
+			if(symbols.snakeHead == field[row][column]){
+				snake.headPOS.row = row
+				snake.tailPOS.column = column
+				break findingSnakeHead
+			}
+		}
+	}
+}
+
+
 	//Maybe make this use paramters as a single json or seomthing?
 function fieldEditor(field,row,column,changeTo,changeFrom = 'DEFAULT'){
 	if(field[row][column] == changeFrom || changeFrom == 'DEFAULT'){ field[row][column] = changeTo }
@@ -44,13 +81,15 @@ function fieldEditor(field,row,column,changeTo,changeFrom = 'DEFAULT'){
 }
 
 generatedTable = generateEmptyField(55,55)
-tablePrinter(generatedTable)
 
-generatedTable = fieldEditor(generatedTable, 41, 40, '1')
+//tablePrinterDelay(generatedTable,1)
+tablePrinterCursor(generatedTable,1)
 
-tablePrinter(generatedTable)
+// generatedTable = fieldEditor(generatedTable, 41, 40, symbols.snakeHead)
+// generatedTable = fieldEditor(generatedTable, 40, 40, symbols.snakeTail)
 
 function tablePrinter(table){
+	process.stdout.write('\033c')
 	var printableTable = []
 	for (var row = 0; row < table.length; row++) {
 		printableTable[row] = ''
@@ -61,4 +100,38 @@ function tablePrinter(table){
 		console.log(printableTable[row])
 	}
 
+}
+
+
+function tablePrinterDelay(table,delay = 0){
+	process.stdout.write('\033c')
+	var printableTable = []
+	for (var row = 0; row < table.length; row++) {
+		printableTable[row] = ''
+		for (var column = 0; column < table[row].length; column++) {
+			printableTable[row] += table[row][column]
+			if(column !== table[row].length-1){printableTable[row] += ' '}
+
+			if(delay !== 0){wait(delay)}
+			process.stdout.write('\033c')
+			for (var rowPrint = 0; rowPrint < printableTable.length; rowPrint++) {
+				console.log(printableTable[rowPrint])
+			}
+
+		}
+	}
+}
+
+function tablePrinterCursor(table,delay = 0){
+	process.stdout.write('\033c')
+	var printableTable = []
+	for (var row = 0; row < table.length; row++) {
+		printableTable[row] = ''
+		for (var column = 0; column < table[row].length; column++) {
+			printableTable[row] += table[row][column]
+			if(delay !== 0){wait(delay)}
+			process.stdout.write('\033[' + (row+1) + ';' + (column*2+1) + 'H' + table[row][column])
+			//process.stdout.write('\033c')
+		}
+	}
 }
